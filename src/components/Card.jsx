8c5ce96fdaf19c027cc5a7112b8419c01f964b80
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-06-24 13:32:51 -0300

    correccion img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ const Card = ({data}) => {
     return (
         <>
             <div className="card">
-      <h3>{data.title}</h3>
-      <h1>US${data.price}</h1>
-      <img src={data.img} alt="IMG   . . ." />
-      <button><h3>Add to Cart</h3></button>
-    </div>
+                <h3>{data.title}</h3>
+                <h1>US${data.price}</h1>
+                <img src={data.image} alt="IMG   . . ." />
+                <button><h3>Add to Cart</h3></button>
+            </div>
         </>
     )
 }
